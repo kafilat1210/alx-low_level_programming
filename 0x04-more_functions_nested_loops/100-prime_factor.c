d5af2a92787068b48fa9271e99379cfc147f35c5
kafilat1210 <cafylhearty@gmail.com>
2023-07-21 16:58:35 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,6 +11,24 @@ int main(void)
 	long int max;
 	long int i;
 	
-	n = 612852475143
-	max = -1
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3, i <= sqrt(n), i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n/i;
+		}
+	}
+	if (n > 2)
+		max = n;
+	printf("%11d\n", max);
+	return (0);
 }
