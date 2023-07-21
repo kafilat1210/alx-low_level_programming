38a7fc447606a7a74e243ae72640a932d1638473
kafilat1210 <cafylhearty@gmail.com>
2023-07-21 03:03:53 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *print_numbers - entry point
+  *
+  *Return: void
+  */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}
