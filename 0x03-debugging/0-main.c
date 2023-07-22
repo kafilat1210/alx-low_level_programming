e1d03feabef9bd7db0985b829225418eda4d153a
OLASUPO KAFILAT <olasupokafilat@OLASUPOs-Air.lan>
2023-07-22 15:41:09 +0100

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- *main - test function for positive and negative
- *
- *Return: Always 0 (Success)
+* main - Test function for positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-				int i;
+	int i;
 
-				i = 0;
-				positive_or_negative(i);
-		return (0);
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+
 }
