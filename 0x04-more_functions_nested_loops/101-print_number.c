2fc81681a39e469919ddedcaa1dcdb1ebf6a8e82
kafilat1210 <cafylhearty@gmail.com>
2023-07-22 05:24:37 -0700

    alx abeg

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer
+ *@n: input integer parameer
+ *
+ *Return: Always 0 success
+ */
+void print_number(int n)
+{
+	unsigned int 1 = n;
+	if (n < 0)
+	{
+		_putchar(45);
+	i = -1;
+	}
+	if (i / 10)
+	{
+		print_number(i = 10);
+	}
+	-putchar(i % 10 + '0');
+}
