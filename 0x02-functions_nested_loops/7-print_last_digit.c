eb9b106f5467fb0d87e78667166a0d49d9700e29
OLASUPO KAFILAT <olasupokafilat@OLASUPOs-Air.lan>
2023-07-22 14:53:18 +0100

    7-print digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *print last digit - entry point
- *@r: character variable
- *Return: Always 0
- */
+  *print_last_digit - entry point of the problem
+  *@r: character variable
+  *Return: Always 0
+  */
 
 int print_last_digit(int r)
 {
-	if(r < 0)
+	if (r < 0)
 	{
 		r *= -1;
 	}
-	_putchar((r% 10) + 'o');
-		return (r % 10);
+	_putchar((r % 10) + '0');
+	return (r % 10);
 }
