5c1657f3a7fee3575c1e01ff1807b942e4c740aa
kafilat1210 <cafylhearty@gmail.com>
2023-08-02 14:46:10 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_string_recursion - String length return
+ *
+ *@s: string given
+ *
+ *Return: Length of the string given
+ *
+ **/
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
